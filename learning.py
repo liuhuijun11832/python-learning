29367a46ae011ceba9d24ecede96d871e0d9fcbd
liuhuijun <liuhuijun2017@gmail.com>
2019-06-05 13:44:25 +0800

    initial python-learning.

---
+++ learning.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import keyword
+print("hello world")
+print("hello ",end="")
+print("world")
+print(keyword.kwlist)
+
+# !/usr/bin/python3
+
+# 第一个注释
+# 第二个注释
+
+'''
+第三注释
+第四注释
+'''
+
+"""
+第五注释
+第六注释
+"""
+
